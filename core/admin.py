6679e992e82f7bc6f33ae625e9fcffddac11687b
Zaki Benaissa <zakben45@gmail.com>
2024-01-12 12:04:20 +0100

    Register ActivityLog model to admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 
-from .models import Session, Semester, NewsAndEvents
+from .models import Session, Semester, NewsAndEvents, ActivityLog
 
 
 admin.site.register(Semester)
 admin.site.register(Session)
 admin.site.register(NewsAndEvents)
+admin.site.register(ActivityLog)
